238e7dcd15a5d180fe6a8371ebd39729d2c7b13c
djohn232 <82186899+djohn232@users.noreply.github.com>
2021-10-15 16:13:25 -0500

    adding POJO classes per instructor feedback needing two

---
+++ src/model/Car.java
@@ -0,0 +1,42 @@
+/**
+ * @author Delanie Johnson - dmjohnson33
+ * CIS175 - Fall 2021
+ * Oct 15, 2021
+ */
+package model;
+
+/**
+ * @author delan
+ *
+ */
+public class Car {
+
+	private int year;
+	private String make;
+	private String model;
+	
+	public int getYear() {
+		return year;
+	}
+	public void setYear(int year) {
+		this.year = year;
+	}
+	public String getMake() {
+		return make;
+	}
+	public void setMake(String make) {
+		this.make = make;
+	}
+	public String getModel() {
+		return model;
+	}
+	public void setModel(String model) {
+		this.model = model;
+	}
+	
+	@Override
+	public String toString() {
+		return "Car [year=" + year + ", make=" + make + ", model=" + model + "]";
+	}
+	
+}

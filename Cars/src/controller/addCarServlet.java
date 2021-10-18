f7765f3cdff48c9ec340a405b383b3bc3b45a07c
djohn232 <82186899+djohn232@users.noreply.github.com>
2021-10-17 21:30:31 -0500

    Update addCarServlet.java

--- Cars/src/controller/addCarServlet.java
+++ Cars/src/controller/addCarServlet.java
@@ -1,11 +1,14 @@
 package controller;
 
-import java.io.IOException;
+import java.io.IOException; 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import model.Car;
+import model.garageDetails;
 
 /**
  * Servlet implementation class addCarServlet
@@ -19,23 +22,20 @@ public class addCarServlet extends HttpServlet {
      */
     public addCarServlet() {
         super();
-        // TODO Auto-generated constructor stub
     }
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
-
+    
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		String make = request.getParameter("Make");
+		String model = request.getParameter("Model");
+		
+		Car li = new Car(make, model);
+		CarHelper dao = new CarHelper();
+		dao.insertItem(li);
+				
+		getServletContext().getRequestDispatcher("/index.html").forward(request, response);
 	}
 
 }

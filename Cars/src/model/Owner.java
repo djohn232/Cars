db7ed09c770c8e840d2ad8b91cf13e439babbf69
djohn232 <82186899+djohn232@users.noreply.github.com>
2021-10-17 21:36:59 -0500

    Update Owner.java

--- Cars/src/model/Owner.java
+++ Cars/src/model/Owner.java
@@ -1,42 +1,27 @@
 /**
  * @author Delanie Johnson - dmjohnson33
  * CIS175 - Fall 2021
- * Oct 15, 2021 (finished by Jacob Krebs 10/17/21)
+ * Oct 15, 2021
  */
 package model;
-
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
 
 /**
  * @author delan
  *
  */
-@Entity
 public class Owner {
-	@Id
-	@GeneratedValue
+	
 	private int id;
 	private String name;
 	private String address;
-
-	//no-arg constructor
-	public Owner() {
-		super();
-	}
+	private int phoneNumber;
 	
-	public Owner(int id, String name, String address) {
-		super();
-		this.id = id;
-		this.name = name;
-		this.address = address;
+	/**
+	 * @param nameToLookUp
+	 */
+	public Owner(String nameToLookUp) {
+		name = nameToLookUp;
 	}
-	
-	public Owner(String name) {
-		this.name = name;
-	}
-	
 	public int getId() {
 		return id;
 	}
@@ -55,13 +40,16 @@ public class Owner {
 	public void setAddress(String address) {
 		this.address = address;
 	}
+	public int getPhoneNumber() {
+		return phoneNumber;
+	}
+	public void setPhoneNumber(int phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
 	
-
-	
-	//Note: removed phone number variable
 	@Override
 	public String toString() {
-		return "Owner [id=" + id + ", name=" + name + ", address=" + address + "]";
+		return "Owner [id=" + id + ", name=" + name + ", address=" + address + ", phoneNumber=" + phoneNumber + "]";
 	}
 
 }
